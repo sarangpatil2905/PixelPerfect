fac0789c87a49c6b9efc8a8ba19b9e9b3fbf1b03
sarang <sarangpatil2905@gmail.com>
2026-02-14 10:03:39 +0530

    Setup

---
+++ PixelPath-frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 8000,
+    cors: {
+      origin: 'http://localhost:8080',
+      credentials: true,
+    },
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+})
